bfda5f867166fe2805f52a9af5b9f650f5886c9d
Reema Adan <reema.1adan@gmail.com>
2024-09-12 17:18:02 -0400

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Name from './component/Name';
 
 function App() {
+  console.log('Come on guys, keept it up!')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1> Reema
+        <Name/>
+      </h1>
       </header>
     </div>
   );
