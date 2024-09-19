426e99835502647deacddd7c98543fb75d25023f
Reema Adan <reema.1adan@gmail.com>
2024-09-19 14:12:37 -0400

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< HEAD
 import Name from './component/Name';
 
 function App() {
@@ -10,6 +11,25 @@ function App() {
       <h1> Reema
         <Name/>
       </h1>
+=======
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+>>>>>>> 2af2fe0 (Initialize project using Create React App)
       </header>
     </div>
   );
