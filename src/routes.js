819efd9e8e5d060e3cead2e3f85af7aa061c74e3
nrahm036 <146502857+nrahm036@users.noreply.github.com>
2024-11-17 14:36:05 -0500

    Route for User PFP Added

--- src/routes.js
+++ src/routes.js
@@ -8,3 +8,23 @@ export const routes = [
   { path: '/spotify-service', component: SpotifyServicePage },
   { path: '/artist-rankings', component: InteractiveArtistRankingsPage },
 ];*/
+
+
+import App from './App';
+import UserProfilePage from './components/UserProfilePage';  
+
+
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+
+  {
+    path: '/profile',
+    element: <UserProfilePage />
+  },
+
+]);
